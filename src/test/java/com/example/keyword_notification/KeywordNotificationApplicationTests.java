abb3a4cc5106ec2a2838a2385b8833fcfe8f239c
robin <mgh3326@naver.com>
2021-06-06 15:27:17 +0900

    Initial commit

---
+++ src/test/java/com/example/keyword_notification/KeywordNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keyword_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordNotificationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
